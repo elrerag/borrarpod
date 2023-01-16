457248889190f4c67ac2d582cc9a360ccb5e6eaa
Luis Herrera <l.herrera.garnica@gmail.com>
2023-01-16 00:11:37 +0000

    inicio

--- entidad/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Pentidad
-
-@admin.register(Pentidad)
-class PentidadAdmin(admin.ModelAdmin):
-    ...
--- entidad/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import generic
-
-from .models import Pentidad
-# Create your views here.
-class EntidadListView(generic.ListView):
-    model="Pentidad"
-
-
